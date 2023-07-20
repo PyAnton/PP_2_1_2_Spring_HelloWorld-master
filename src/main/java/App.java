242cf03134e00824fae7d468bd7e415e348757f1
Antonmak112 <pyanton112@gmail.com>
2023-07-20 16:58:21 +0500

    yes

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        System.out.println(bean2.getMessage());
+        System.out.println(bean1 == bean2);
+        Cat cat1 = (Cat) applicationContext.getBean("Cat");
+        Cat cat2 = (Cat) applicationContext.getBean("Cat");
+        System.out.println(cat1.getName());
+        System.out.println(cat2.getName());
+        System.out.println(cat1 == cat2);
+
+    }
+}
